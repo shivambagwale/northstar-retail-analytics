14bdaf2a5cc587c69a253995847a9f6ea0a43d4f
shivambagwale <shivambagwale@gmail.com>
2026-05-05 21:38:03 +0530

    Update queries.sql
    added first revenue analysis query

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,1 +1,13 @@
+USE northstar_retail;
 
+SELECT
+    s.city,
+    p.product_name,
+    SUM(st.total_amount) AS revenue
+FROM sales_transactions st
+JOIN stores s
+    ON st.store_id = s.store_id
+JOIN products p
+    ON st.product_id = p.product_id
+GROUP BY s.city, p.product_name
+ORDER BY revenue DESC;
